2f52a1a2000e2fef36199dba9522049bdfe9e3a5
Guille <sokerwild96@gmail.com>
2022-02-24 17:28:13 +0100

    Parte 1 funcionando
    Hasta la pagina 20 todo bien

--- Comportamientos_P3/Intermediate/Build/Win64/Comportamientos_P3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++ Comportamientos_P3/Intermediate/Build/Win64/Comportamientos_P3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -1,2 +1,2 @@
-// Compiler: 14.29.30140
-#include "C:/Users/Soker/Desktop/PRACTICAS_UI/Comportamientos_P3/Intermediate/Build/Win64/Comportamientos_P3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
+// Compiler: 14.28.29915
+#include "C:/Users/guillermo.suarez/Documents/GitHub/Comportamientos/Comportamientos_P3/Intermediate/Build/Win64/Comportamientos_P3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
